666ca842595376aac29d064c3381aeaa32d93ded
Christopher Pitt <cgpitt@gmail.com>
2018-06-13 17:02:24 +0200

    Add storage and bounds memory

--- source/index.js
+++ source/index.js
@@ -1,20 +1,41 @@
 const { app, BrowserWindow } = require("electron")
+const { Store } = require("./store")
+
+const store = new Store({
+    name: "preferences",
+    defaults: {
+        bounds: { width: 800, height: 600 },
+    },
+})
+
+let window = null
 
 function createWindow() {
-    const window = new BrowserWindow({ width: 800, height: 600 })
+    window = new BrowserWindow(store.get("bounds"))
+
+    window.on("resize", () => {
+        store.set("bounds", window.getBounds())
+    })
+
+    window.on("move", () => {
+        store.set("bounds", window.getBounds())
+    })
+
     window.loadFile("./build/index.html")
 }
 
 app.on("ready", createWindow)
 
 app.on("window-all-closed", () => {
+    window = null
+
     if (process.platform !== "darwin") {
         app.quit()
     }
 })
 
 app.on("activate", () => {
-    if (win === null) {
+    if (window === null) {
         createWindow()
     }
 })
---
+++ source/store.js
@@ -0,0 +1,33 @@
+const electron = require("electron")
+const path = require("path")
+const fs = require("fs")
+
+class Store {
+    constructor({ name, defaults }) {
+        const dataPath = (electron.app || electron.remote.app).getPath(
+            "userData",
+        )
+
+        this.path = path.join(dataPath, name + ".json")
+        this.data = parseDataFile(this.path, defaults)
+    }
+
+    get(key) {
+        return this.data[key]
+    }
+
+    set(key, value) {
+        this.data[key] = value
+        fs.writeFileSync(this.path, JSON.stringify(this.data))
+    }
+}
+
+function parseDataFile(filePath, defaults) {
+    try {
+        return JSON.parse(fs.readFileSync(filePath))
+    } catch (error) {
+        return defaults
+    }
+}
+
+module.exports = { Store }
